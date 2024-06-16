9a1f05d4f5c9b2df59357c068850b1b1d92e4e2f
SamuelLindao <75032911+SamuelLindao@users.noreply.github.com>
2024-06-16 12:48:36 -0300

    Files

--- main.py
+++ main.py
@@ -1,19 +1,80 @@
-from qiskit import QuantumCircuit
-from qiskit.quantum_info import SparsePauliOp
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit import QuantumCircuit
+from qiskit_ibm_runtime import EstimatorOptions
 from qiskit_ibm_runtime import EstimatorV2 as Estimator
+import matplotlib.pyplot as plt
+import numpy as np
+from qiskit_ibm_runtime import QiskitRuntimeService
 
-# Create a new circuit with two qubits
-qc = QuantumCircuit(2)
+def get_qc_for_n_qubit_GHZ_state(n: int) -> QuantumCircuit:
+    """This function will create a qiskit.QuantumCircuit (qc) for an n-qubit GHZ state.
 
-# Add a Hadamard gate to qubit 0
-qc.h(0)
+    Args:
+        n (int): Number of qubits in the n-qubit GHZ state
 
-# Perform a controlled-X gate on qubit 1, controlled by qubit 0
-qc.cx(0, 1)
+    Returns:
+        QuantumCircuit: Quantum circuit that generate the n-qubit GHZ state, assuming all qubits start in the 0 state
+    """
+    if isinstance(n, int) and n >= 2:
+        qc = QuantumCircuit(n)
+        qc.h(0)
+        for i in range(n - 1):
+            qc.cx(i, i + 1)
+    else:
+        raise Exception("n is not a valid input")
+    return qc
 
-# Return a drawing of the circuit using MatPlotLib ("mpl"). This is the
-# last line of the cell, so the drawing appears in the cell output.
-# Remove the "mpl" argument to get a text drawing.
-qc.draw("mpl")
-print(qc.draw())
+
+# Create a new circuit with two qubits (first argument) and two classical
+# bits (second argument)
+n = 100
+qc = get_qc_for_n_qubit_GHZ_state(n)
+from qiskit.quantum_info import SparsePauliOp
+
+# ZZII...II, ZIZI...II, ... , ZIII...IZ
+operator_strings = ['Z' + 'I' * i + 'Z' + 'I' * (n - 2 - i) for i in range(n - 1)]
+print(operator_strings)
+print(len(operator_strings))
+
+operators = [SparsePauliOp(operator) for operator in operator_strings]
+# If you did not previously save your credentials, use the following line instead:
+# service = QiskitRuntimeService(channel="ibm_quantum", token="<MY_IBM_QUANTUM_TOKEN>")
+service = QiskitRuntimeService(channel="ibm_quantum", token = "cd7187e5a8139061c27fde154da6d4d1a0293eae993a15be34e39282a6f4d773c028bcfe362a38a852970975603b2b59a71a4bf1bac1b37ca3a323e30d3354b5")
+
+backend = service.least_busy(simulator=False, operational=True, min_num_qubits=100)
+pm = generate_preset_pass_manager(optimization_level=1, backend=backend)
+
+isa_circuit = pm.run(qc)
+isa_operators_list = [op.apply_layout(isa_circuit.layout) for op in operators]
+
+options = EstimatorOptions()
+options.resilience_level = 1
+options.optimization_level = 0
+options.dynamical_decoupling.enable = True
+
+
+
+
+# Create an Estimator object
+estimator = Estimator(backend, options=options)
+
+# Submit the circuit to Estimator
+job = estimator.run([(isa_circuit, isa_operators_list)])
+job_id = job.job_id()
+print(job_id)
+
+# data
+data = list(range(1, len(operators)+1)) # Distance between the Z operators
+result = job.result()[0] # Use for EstimatorV2.
+values = result.data.evs # Expectation value at each Z operator. Use for Estimator V2.
+# values = job.result().values # Use for EstimatorV1
+values = [v / values[0] for v in values] # Normalize the expectation values to evaluate how they decay with distance.
+
+# plotting graph
+plt.scatter(data, values, marker='o', label='100-qubit GHZ state')
+plt.xlabel('Distance between qubits $i$')
+plt.ylabel(r'$\langle Z_0 Z_i \rangle / \langle Z_0 Z_1 \rangle $')
+plt.legend()
+plt.show()
+options.dynamical_decoupling.sequence_type = "XY4"
